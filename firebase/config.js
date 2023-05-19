6ab7d2a291557b2e62b6c42810902616b225ce8b
Cameron Morrongiello <cameronmorrongiello@Camerons-MacBook-Pro-3.local>
2023-05-19 12:24:06 -0600

    debug

--- firebase/config.js
+++ firebase/config.js
@@ -2,12 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 import { FIREBASE_API } from "../config-global";
+console.log(FIREBASE_API);
 
 // Initialize Firebase
 const app = initializeApp(FIREBASE_API);
 
+console.log(app);
+
 // Initalize Firesore
 const db = getFirestore(app);
+
+console.log(db);
 
 module.exports = {
   db,
